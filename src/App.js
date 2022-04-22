a79f41d6e48abce5c0ea50dad624d5d11c17b1ad
robinrakibul <robinrakibulhassan@gmail.com>
2022-04-22 16:56:11 +0600

    added update user componenet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AddUser from './components/AddUser/AddUser';
 import Home from './components/Home/Home';
+import UpdateUser from './components/UpdateUser/UpdateUser';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="adduser" element={<AddUser></AddUser>}></Route>
       <Route path="/" element={<Home></Home>}></Route>
+      <Route path="/update" element={<UpdateUser></UpdateUser>}></Route>
     </Routes>
     </div>
   );
